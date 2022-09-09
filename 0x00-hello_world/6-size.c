5b1f216dfeb9b77e011b2a0ac8ff636c4a10e73b
blarrklad <blarrklad@gmail.com>
2022-09-08 20:08:24 -0700

    Task commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	float f;
+	printf("Size of a char: %lu byte(s).\n", sizeof(i));
+	printf("Size of an int: %lu byte(s).\n", sizeof(d));
+	printf("Size of a long int: %lu byte(s).\n", (long)sizeof(i));
+	printf("Size of a long long int: %lu byte(s).\n", (long long)sizeof(i));
+	printf("Size of a float: %lu byte(s).\n", sizeof(f));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,4 +4,4 @@ Task 2: Generates the assembly code of a C code and save it in an output file. <
 Task 3: Compiles a C file and creates an executable named cisfun. <br>
 Task 4: Prints exactly "Programming is like building a multilingual puzzle, followed by a new line. <br>
 Task 5: Prints exactly "with proper grammar, but the outcome is a piece of art,", followed by a new line. <br>
-
+Task 6: Prints the size of various types on the computer it is compiled and run on. <br>
