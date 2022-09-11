b25a306531157db3b48292e932b8b09d2cf83922
blarrklad <blarrklad@gmail.com>
2022-09-11 16:31:51 -0700

    Task commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	
-	printf("Size of a: %i byte(s).\n", sizeof(a));
-	printf("Size of a: %i byte(s).\n", sizeof(b));
-	printf("Size of a: %i byte(s).\n", sizeof(c));
-	printf("Size of a: %i byte(s).\n", sizeof(d));
-	printf("Size of a: %i byte(s).\n", sizeof(e));
+
+	printf("Size of a char: %i byte(s)", sizeof(a));
+	printf("Size of an int: %i byte(s)", sizeof(b));
+	printf("Size of a long int: %i byte(s)", sizeof(c));
+	printf("Size of a long long int: %i byte(s)", sizeof(d));
+	printf("Size of a float: %i byte(s)", sizeof(e));
+
 }
