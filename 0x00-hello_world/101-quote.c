1a42876dca624799f42fb1ef8ce4263fc6e65495
blarrklad <blarrklad@gmail.com>
2022-09-11 15:47:29 -0700

    Task commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
