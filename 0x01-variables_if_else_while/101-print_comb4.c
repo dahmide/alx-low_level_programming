8bc9948370899e54750cb2d5d85c5c776e5ef5c5
blarrklad <blarrklad@gmail.com>
2022-09-12 16:48:34 -0700

    Task commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (digit_1 = 0; digit_1 < 8; digit_1++)
 	{
-		for (digit_2 = digit_1 + 1, digit_2 < 9; digit_2++)
+		for (digit_2 = digit_1 + 1; digit_2 < 9; digit_2++)
 		{
 			for (digit_3 = digit_2 + 1; digit_3 < 10; digit_3)
 			{
