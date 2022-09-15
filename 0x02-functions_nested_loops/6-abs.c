686f8a3c9896dc867803b90266b5e6a6b2e57dee
blarrklad <blarrklad@gmail.com>
2022-09-15 14:03:09 -0700

    Task commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _isabs - Entry point
+ * 
+ * Description: Computes the absolute value of an integer
+ * Return: Absolute value
+ */
+
+int _isabs(int)
+{
+	return (abs());
+}
