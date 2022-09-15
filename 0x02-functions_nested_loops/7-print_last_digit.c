44169a6049dc5cc20e13c66577ff5292d2aacd40
blarrklad <blarrklad@gmail.com>
2022-09-15 14:17:57 -0700

    Task commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: Int params
+ *
+ * Description: Print the last digit of a number
+ * Return: Last digit
+ */
+
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	return (x);
+}
