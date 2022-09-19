5bb15f66c73a3c636875143f7853f546fd906bcf
blarrklad <blarrklad@gmail.com>
2022-09-19 13:32:42 -0700

    Task commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * void times_table(void) - Entry point
+ *
+ * Description: Print the 9 times table, starting with 0
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i = 0;
+	int k = 0;
+
+	while (i <= 9)
+	{
+		while (k <= 9)
+		{
+
+			int j = i * k;
+			_putchar(k);
+			_putchar(',');
+			_putchar(' ');
+
+			k++;
+		}
+
+		i++;
+	}
+}
