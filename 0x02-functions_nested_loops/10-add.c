356b8c4878354a9629e0ab3783ceee03a5374e66
blarrklad <blarrklad@gmail.com>
2022-09-19 17:35:37 -0700

    Task commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: 1st integer
+ * @b: 2nd Integer
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
